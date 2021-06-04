0446aaf773f46cb09da1e4046e0514b9bd50a4b9
gnamestnik <gnamestnik@DESKTOP-M45P1A7>
2021-06-04 13:49:01 +0800

    Added new feature now opens in default browser  Moving away from allowing custom commands to be run executed instead  Still can  use agent commands, but currently this is now hardcoded not to

--- internal/sshrimpagent/sshrimpagent.go
+++ internal/sshrimpagent/sshrimpagent.go
@@ -24,13 +24,14 @@ type sshrimpAgent struct {
 func NewSSHrimpAgent(c *config.SSHrimp, signer ssh.Signer) agent.Agent {
 
 	providerConfig := provider.ProviderConfig{
-		ClientID:     c.Agent.ClientID,
-		ClientSecret: c.Agent.ClientSecret,
-		ProviderURL:  c.Agent.ProviderURL,
-		PKCE:         true,
-		Nonce:        true,
-		AgentCommand: c.Agent.BrowserCommand,
-		// ProviderReturnHTML: "<HTML><HEAD></HEAD><BODY onload=\"window.open('', '_self', ''); window.close();\">Successfully Logged in, you can close this page</BODY></HTML>",
+		ClientID:               c.Agent.ClientID,
+		ClientSecret:           c.Agent.ClientSecret,
+		ProviderURL:            c.Agent.ProviderURL,
+		PKCE:                   true,
+		Nonce:                  true,
+		AgentCommand:           c.Agent.BrowserCommand,
+		ProviderReturnHTML:     "<HTML><HEAD></HEAD><BODY onload=\"window.open('', '_self', ''); window.close();\">Successfully Logged in, you can close this page</BODY></HTML>",
+		AgentUseDefaultBrowser: true,
 	}
 
 	return &sshrimpAgent{
